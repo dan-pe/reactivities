2bb477baa3e4395687ce5783c9f62007069f827b
Daniel_Perwejnis <daniel.perwejnis@gmail.com>
2020-01-30 14:00:12 +0100

    Initial commit - skeleton solution

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            var values = await context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await context.Values.FirstOrDefaultAsync(v => v.Id.Equals(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] string value)
+        {
+            await context.Values.AddAsync(new Domain.Value { Name = value });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Persistance/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                new Value { Id = 1, Name = "value1" },
+                new Value { Id = 2, Name = "value2" },
+                new Value { Id = 3, Name = "value3" }
+                );
+        }
+    }
+}
