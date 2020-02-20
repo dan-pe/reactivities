b227b9ea68df413b4d623a5cfda2b703f15dc4af
Daniel_Perwejnis <daniel.perwejnis@gmail.com>
2020-02-20 16:00:24 +0100

    Add activity controller using MediatR

---
+++ Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public ActivitiesController(IMediator mediatoR)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> Get()
+        {
+            return await mediator.Send(new List.Query());
+        }
+    }
+}
---
+++ Application/Activities/List.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext context;
+
+            public Handler(DataContext dataContext)
+            {
+                context = dataContext;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken) =>
+                await context.Activities.ToListAsync();
+        }
+    }
+}
