92a281099c95d1ecf289f5bcaf249ec855b4d2f6
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2020-03-29 14:50:17 +0200

    Activity Details breakdown into separate compontenets

---
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActivityDetailedChat: React.FC = () => {
+  return <h1>Chat</h1>;
+};
+
+export default ActivityDetailedChat;
---
+++ client-app/src/features/activities/details/ActivityDetailedSiderbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActivityDetailedSiderbar = () => {
+  return <h1>Sidebar</h1>;
+};
+
+export default ActivityDetailedSiderbar;
